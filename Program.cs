48c381af52a6636ef8221df71201fd560e7d4bc3
MaxVortman <maka.abort@gmail.com>
2018-11-21 01:35:41 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lox_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Lox\#
+
+This is a C\# implementation of Lox programming language guided folowing tutorial craftinginterpreters.com
