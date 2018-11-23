b8a0cc931e47288aac3557b7dd10883e8f4449a1
MaxVortman <maka.abort@gmail.com>
2018-11-23 11:54:32 +0300

    AstPrinter added

---
+++ loxsharp/AstPrinter.cs
@@ -0,0 +1,50 @@
+using System.Text;
+
+namespace Lox_
+{
+    // Creates an unambiguous, if ugly, string representation of AST nodes.
+    class AstPrinter : Expr.Visitor<string>
+    {
+        internal string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+
+        public string VisitBinaryExpr(Expr.Binary expr)
+        {
+            return Parenthesize(expr.op.lexeme, expr.left, expr.right);
+        }
+
+        public string VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Parenthesize("group", expr.expression);
+        }
+
+        public string VisitLiteralExpr(Expr.Literal expr)
+        {
+            if (expr.value == null) return "nil";
+            return expr.value.toString();
+        }
+
+        public string VisitUnaryExpr(Expr.Unary expr)
+        {
+            return Parenthesize(expr.op.lexeme, expr.right);
+        }
+
+        private string Parenthesize(string name, params Expr exprs)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
